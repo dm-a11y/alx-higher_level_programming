9996ca1f0f386b8cc66e9508a651e2db4c7cf560
Daneil <daneilmehabaw55@gmail.com>
2023-04-27 01:29:21 -0700

    if else, loops, functions

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a program that prints the ASCII alphabet, in lowercase, not followed by \n"""
+"""a program that prints the ASCII alphabet, in lowercase, not followed by\n"""
 for i in range(ord('a'), ord('z') + 1):
     print("{:s}".format(chr(i)), end="")
 
