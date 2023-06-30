abaf5c3cc396a64b8d06bf32fcc901f7e9a237c4
Daneil <daneilmehabaw55@gmail.com>
2023-06-30 14:06:39 -0700

    python network 1 mandatory tasks

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL
+sends a request to the URL and displays
+the body of the response.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
